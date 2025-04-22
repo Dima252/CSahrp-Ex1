c282e1271cbb9a32c7a76803a8c44f2a8e9632f4
danny todoseyev <todoseyev.danny@gmail.com>
2025-04-22 21:24:08 +0300

    number4almost

---
+++ Ex01 Shalev 322582008 Dima 322810060.sln/Ex01_05/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex01_05
+{
+    class Program
+    {
+        static void Main()
+        {
+            string input;
+            bool inputIsValid = false;
+
+            Console.WriteLine("Please enter integer with 8 digit's");
+
+            while (inputIsValid == false)
+            {
+                input = Console.ReadLine();
+                if (input.Length == 8)
+                {
+                    inputIsValid = true;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input, Please enter a valid number:");
+                }
+            }
+
+
+
+        }
+    }
+}
